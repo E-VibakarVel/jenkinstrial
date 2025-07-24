9b021abd411985eba1350de83eee8c89953f5714
Vibakar Vel E <vibakarvel.e@tcs.com>
2025-07-24 14:45:40 +0530

    #Refs intiial boot commit

---
+++ src/main/java/com/example/jenkinstrial/JenkinstrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinstrial/JenkinstrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
