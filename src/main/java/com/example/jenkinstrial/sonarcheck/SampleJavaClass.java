35633457e44f98d44826cdb2c321d2250afb0e21
Vibakar Vel E <vibakarvel.e@tcs.com>
2025-08-19 11:18:12 +0530

    so

--- src/main/java/com/example/jenkinstrial/sonarcheck/SampleJavaClass.java
+++ src/main/java/com/example/jenkinstrial/sonarcheck/SampleJavaClass.java
@@ -4,5 +4,6 @@ public class SampleJavaClass {
 
     public static  void main(){
         System.out.println("Sonar issue for sop");
+        System.out.println("Sonar issue for sonarqube new");
     }
 }
