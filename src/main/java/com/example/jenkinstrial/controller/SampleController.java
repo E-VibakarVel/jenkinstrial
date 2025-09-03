3576f89b22362b6b1c037d37393a11cd6a7dba48
Vibakar Vel E <vibakarvel.e@tcs.com>
2025-09-03 15:30:00 +0530

    #refs boot to spring mvc conversion

--- src/main/java/com/example/jenkinstrial/controller/SampleController.java
+++ src/main/java/com/example/jenkinstrial/controller/SampleController.java
@@ -3,12 +3,15 @@ package com.example.jenkinstrial.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @GetMapping("/jenkins")
+    @RequestMapping("/jenkins")
+    @ResponseBody
     public String welcome() {
         return "Jenkins "; // This refers to a view named "welcome" (e.g., welcome.jsp or welcome.html)
     }
