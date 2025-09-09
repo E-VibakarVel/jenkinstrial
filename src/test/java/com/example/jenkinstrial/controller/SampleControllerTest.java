df7d63b51515d8b327f960a29f9479f9009b561b
Vibakar Vel E <vibakarvel.e@tcs.com>
2025-09-09 16:42:30 +0530

    #Refs fix

--- src/test/java/com/example/jenkinstrial/controller/SampleControllerTest.java
+++ src/test/java/com/example/jenkinstrial/controller/SampleControllerTest.java
@@ -1,27 +1,41 @@
 package com.example.jenkinstrial.controller;
 
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations ={"classpath:/WEB-INF/spring-servlet.xml"} )
 public class SampleControllerTest {
 
+    @Autowired
+    private WebApplicationContext wac;
     private MockMvc mockMvc;
 
-    @BeforeEach
-    public void setup(){
-        SampleController sampleController = new SampleController();
-        mockMvc = MockMvcBuilders.standaloneSetup(sampleController).build();
 
+    @Before
+    public void setup(){
+        System.out.println(wac);
+this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     @Test
     public void testSayHello() throws Exception{
-        mockMvc.perform(get("/jenkins")).andExpect(status().isOk())
+        this.mockMvc.perform(get("/jenkins")).andExpect(status().isOk())
                 .andExpect(content().string("Jenkins"));
     }
 
